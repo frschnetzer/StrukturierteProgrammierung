4151aed75d50f606c96db1d3951beaabc53352f5
Franziska <83272870+frschnetzer@users.noreply.github.com>
2024-11-09 11:46:25 +0100

    made changes to coin game

--- Uebungen/Uebungsblatt_02/coin_game.py
+++ Uebungen/Uebungsblatt_02/coin_game.py
@@ -1,10 +1,4 @@
 def main():
-    def switch(input):
-        if input:
-            return False
-        else:
-            return True
-   
     def coin_game():        
         firstline = [1,2,3]
         secondline = [4,5,6,7]
@@ -12,7 +6,7 @@ def main():
         isPlayer1 = True
 
         while firstline or secondline or thirdline: #solange mind eine liste noch ein element beinhaltet
-            entries = list(map(int, input().split())) # get input as list of int
+            entries = list(map(int, input().split())) # get input as list of int                  
             for i in entries: # iterating throught the input list
                 if i in firstline: # check if i is in one of the lists
                     firstline.remove(i) # remove i
@@ -20,8 +14,10 @@ def main():
                     secondline.remove(i)
                 else:
                     thirdline.remove(i)
-            switch(isPlayer1)
-        switch(isPlayer1)
+            if isPlayer1:
+                isPlayer1 = False
+            else:
+                isPlayer1 = True
         if isPlayer1:
             return "Player 1"
         return "Player 2"
